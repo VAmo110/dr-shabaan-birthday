9647721c9ce44683fe5c558e2e6f4ff81d81fc8b
VAmo110 <bbad01133@gmail.com>
2025-05-23 21:14:21 +0300

    ✅ Fix: postcss config cleaned

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-=======
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
->>>>>>> 5327f6b3b6e8b57f65826a88d674e934c60c91b5
 };

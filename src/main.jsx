945d52ed41ef63a213a4d6d734858552c6bac7e4
VAmo110 <bbad01133@gmail.com>
2025-05-23 21:20:40 +0300

    ✅ Fix: removed duplicate code in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -8,15 +7,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> 5327f6b3b6e8b57f65826a88d674e934c60c91b5
 );
